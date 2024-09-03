d3748e0109db26f32e4f691a355c68218b9321b2
tur3ani <mohmadnturani@gmail.com>
2024-09-03 11:34:12 +0300

    cleaned up the code and made it more OOB

---
+++ sprites.py
@@ -0,0 +1,22 @@
+from os.path import join
+import pygame
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(join('images', 'player.png')).convert_alpha()
+        self.rect = self.image.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+        self.direction = pygame.math.Vector2()
+        self.speed = 500
+
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+        self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+
+        if self.direction.length() != 0:
+            self.direction.normalize_ip()
+
+        self.rect.center += self.direction * self.speed * dt
